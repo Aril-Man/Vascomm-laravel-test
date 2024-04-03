711b916a7302658941076dbfd1a48d79d6d81ea8
Aril-Man <azrielfhr23@gmail.com>
2024-04-04 01:41:37 +0700

    update: condition landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,8 +10,10 @@
 <body>
    @auth
         <p>{{ Auth::user()->name }}</p>
+        <a href="{{ route('logout') }}" class="btn btn-primary">Logout</a>
+    @else
+        <a href="{{ route('login') }}" class="btn btn-primary">LOGIN</a>
+        <a href="{{ route('registerPage') }}" class="btn btn-primary">Register</a>
    @endauth
-    <a href="{{ route('login') }}" class="btn btn-primary">LOGIN</a>
-    <a href="{{ route('registerPage') }}" class="btn btn-primary">Register</a>
 </body>
 </html>
